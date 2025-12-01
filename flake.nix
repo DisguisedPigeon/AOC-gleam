4648f46ef6905a56335497f99e4336257318ebe8
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2025-12-01 22:41:04 +0100

    2025 day1, fix day 9 stuff

--- flake.nix
+++ flake.nix
@@ -1,30 +1,52 @@
 {
-  description = "Gleam dev shell";
+  description = "Gleam dev environment for Entomologist";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
+
   outputs =
-    { systems, nixpkgs, ... }:
-    let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs =
-            with pkgs;
-            (
-              [
-                watchexec
-                gleam
-                rebar3
-                beam.interpreters.erlang_27
-                nixd
-              ]
-              ++ lib.optional stdenv.isLinux inotify-tools
-            );
+    inputs@{ ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (_: {
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt-tree;
+          devShells.default = pkgs.mkShell {
+            shellHook = ''
+              export AOC_COOKIE="[REDACTED]"
+            '';
+            buildInputs = with pkgs; [
+              # -- DEVENV --
+              # Gleam compiler and tooling
+              gleam
+
+
+              # Erlang stuff
+              beamMinimal27Packages.erlang
+              beamMinimal27Packages.rebar3
+              beamMinimal27Packages.erlfmt
+              erlang-language-platform
+
+              # Executes a command when a file change is detected
+              watchexec
+
+              # Pre-commit hook helper
+              pre-commit
+
+            ]
+            ++ lib.optional stdenv.isLinux inotify-tools;
+          };
         };
-      });
-    };
+
+    });
 }
